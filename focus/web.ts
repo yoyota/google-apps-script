dcc2843e13d344d821826e708349cb038efef220
YongTak Yoo <yongtaky@gmail.com>
2021-09-07 15:29:44 +0900

    Add onClick event listner

--- focus/web.ts
+++ focus/web.ts
@@ -1,3 +1,11 @@
+import { getCalendar } from "./calendar"
+
 function doGet() {
   return HtmlService.createHtmlOutputFromFile("index")
+}
+
+function doCreateEvent(title: string) {
+  const calendar = getCalendar()
+  const now = new Date()
+  calendar.createEvent(title, now, now)
 }
